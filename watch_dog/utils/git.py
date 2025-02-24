fccfd49086b7c6bb39b93c2cf161ca280f220ee6
Israel Shedrack <israelshedrack913@gmail.com>
2025-02-24 11:11:08 +0100

    update:

--- watch_dog/utils/git.py
+++ watch_dog/utils/git.py
@@ -34,41 +34,23 @@ def handle_pull(branch_name):
 class Git_service:
 
     def git_push(self):
-        # get commit message, get branch name
-        message = input('Enter your commit message: ')
-        try:
-            with open('details.json', 'r') as old_branch:
-                branch_file = json.load(old_branch)
+        message = input("Enter your commit message: ")
 
-                branch = branch_file['branch_name']
+        try: 
+            with open('details.json', 'r') as json_file:
+                loaded_data = json.load(json_file)
+                branch = loaded_data['branch_name']
 
-        except KeyError:
-            branch = None
-        except json.decoder.JSONDecodeError:
-            branch = None
-
+        except KeyError or json.decoder.JSONDecodeError:
+            branch = False
+        print(branch)
+        
         files = listdir()
-        if branch:
-            print('sdf')
 
-        if ".git" in files and branch:
-            handle_update(message, branch)
-            print('git push')
+        if ".git" in files:
+            handle_update(str(message), str(branch))
 
-        else:
 
-            new_branch = input('Preferred branch name: ')
-            url = input("Enter branch url: ")
-            data = {
-                'branch_name' : str(new_branch)
-            }
-
-            with open('details.json', 'w') as json_file:
-                json.dump(data, json_file, indent=4)
-
-            handle_init(message, url, new_branch)
-            handle_update(message, new_branch)
-            print('git pushed')
 
     def git_pull(self):
         new_branch = input('Preferred branch name: ')
